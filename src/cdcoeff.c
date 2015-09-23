1cfa0860b14a6714771555197acb6b6d68e16693
Weijia Sun <swj@mail.iggcas.ac.cn>
2015-09-23 12:18:26 +1000

    coding coeff for regular grid done

--- src/cdcoeff.c
+++ src/cdcoeff.c
@@ -27,7 +27,7 @@ int factorial(int m)
 @param[in] m: order of the derivatives, m=1,2,3,4
 @param[in] n: order of spatial accuray, e.g., n=4, 10
 */
-void cdcoeff(int m, int n)
+void cdcoeff(int m, int n, double *cc)
 {
 	m = abs(m);
 	n = abs(n);
@@ -41,42 +41,55 @@ void cdcoeff(int m, int n)
 	double complex tmp, tmp1, tmp2;
 	double complex sum;
 	
-	tmp = cpow(I,m)*factorial(m)*0.5/PI;
-	
-	printf("tmp, %f, %fi\n", creal(tmp), cimag(tmp));
-	printf("PI=%f\n", PI);
-	
+	tmp = pow(I,m)*factorial(m)*0.5/PI;
+		
 	nn = n;
 	for(n=1; n<=nn; n++)
 	{
 		sum = 0.0;
 		for(k=0; k<=m; k++)
 		{
-			tmp1 = cexp(I*kn*n)+pow(-1, m-k+1)*cexp(-I*kn*n);
-			tmp2 = factorial(m-k)*cpow(n*I, k+1);
+			tmp1 = exp(I*kn*n)+pow(-1, m-k+1)*exp(-I*kn*n);
+			tmp2 = factorial(m-k)*pow(n*I, k+1);
 		
 			sum += pow(-1.0, k)*pow(kn, m-k)*tmp1/tmp2;
 			
-			printf("tmp1=,%f, %f\n", creal(tmp1), cimag(tmp1));
-			printf("tmp2=,%f, %f\n", creal(tmp2), cimag(tmp2));
-			printf("sum=,%f, %f\n", creal(sum), cimag(sum));
+//			printf("tmp1=,%f, %f\n", creal(tmp1), cimag(tmp1));
+//			printf("tmp2=,%f, %f\n", creal(tmp2), cimag(tmp2));
+//			printf("sum=,%f, %f\n", creal(sum), cimag(sum));
 
 		}
 		tmp2 = tmp*sum;
-		printf("n=%d, coeff=%f\n\n", n, creal(tmp*sum));
+		cc[n] = creal(tmp2);
+//		printf("n=%d, coeff=%f\n", n, cc[n]);
 	}
 	
+	// n=0
+	tmp = pow(I, m)*pow(kn, m+1)*0.5/PI*(1+pow(-1,m))/(m+1);
+	cc[0] = creal(tmp);
+	
+	for(n=0; n<=nn; n++)
+	{
+		printf("cc[%d]=%f\n", n, cc[n]);
+	}
 	
 	return;
 }
 
+#define NOMAIN
+#ifdef MAIN
+
 int main()
 {
-	int m=1;
+	int m=2;
+	int n=10;
 	int result;
+	double cc[2*n+1];
 	
 	result = factorial(m);
 	printf("result=%d\n", result);
 	
-	cdcoeff(m,3);
+	cdcoeff(m,n,cc);
 }
+
+#endif
